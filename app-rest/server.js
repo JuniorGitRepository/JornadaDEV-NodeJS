98a5fed2b665c82d5731f31cc3b60f4fe662c6db
José Fernandes da Silva Júnior <juniorcursosonline@outlook.com>
2024-11-14 09:56:14 -0300

    teste

--- app-rest/server.js
+++ app-rest/server.js
@@ -1,9 +1,9 @@
 import app from './src/app.js'
-const porta = 3000
+const PORTA = 3000
 
 
 //escultar a porta 3000
-app.listen(porta,()=>{
-    console.log(`Servidor rodando no endereço http://localhost:${porta}`)
+app.listen(PORTA,()=>{
+    console.log(`Servidor rodando no endereço http://localhost:${PORTA}`)
 })
                
--- app-rest/src/app.js
+++ app-rest/src/app.js
@@ -1,10 +1,20 @@
 import express from 'express'
 const app = express()
 
+//mock
+const selecoes =[
+    {id:1, selecao:'Brasil', grupo:'G'},
+    {id:2, selecao:'Suíça', grupo:'G'},
+    {id:3, selecao:'Servia', grupo:'G'},
+    {id:4, selecao:'Camarões', grupo:'G'},
+]
 
 //criar rota padrão ou raiz
 app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
 
+app.get('/selecoes',(req,res)=>{
+    res.status(200).send(selecoes)
+})
 export  default app
